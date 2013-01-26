f9587c0165409663a3f2ae90781ba849f1a5c2f0
Lee Treveil <leetreveil@gmail.com>
2013-01-26 14:47:12 +0000

    update setup file

--- setup.py
+++ setup.py
@@ -2,7 +2,9 @@ from distutils.core import setup
 
 setup(
     name = 's3upload',
-    version = '0.2.0',
-    py_modules=['s3upload'],
+    version = '0.2.1',
+    py_modules = ['s3upload'],
+    author = 'Lee Treveil',
+    author_email = 'leetreveil@gmail.com'
     description = 'A python module for uploading data to s3 using the s3 multipart API',
 )
